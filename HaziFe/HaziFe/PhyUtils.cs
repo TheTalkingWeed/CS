a350d26abf7d776e6f7b0f0219df2cf403582df3
Alex_sz1 <61713353+TheTalkingWeed@users.noreply.github.com>
2021-12-07 12:47:36 +0100

    Add files via upload

---
+++ HaziFe/HaziFe/PhyUtils.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HaziFe
+{
+    public class PhyUtils
+    {
+        public static List<int> Ranger(int also, int felso, int lepeskoz)
+        {
+            List<int> output = new List<int>();
+
+            for (int i = also; i < felso; i+=lepeskoz)
+            {
+                output.Add(i);
+            }
+            
+            
+            return output;
+        }
+
+       public static List<int> Ranger(int also,int felso)
+        {
+            return Ranger(also, felso, 1);
+        }
+
+        public static List<int> Ranger(int felso)
+        {
+            return Ranger(0, felso, 1);
+        }
+
+        public static int ReverseInt(int a)
+        {
+            return a.ToString().Reverse().ToInt();
+        }
+    }
+}
---
+++ HaziFe/HaziFe/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HaziFe
+{
+    public static class StringExtensions
+    {
+        public static int ToInt(this string str)
+        {
+            return int.Parse(str);
+        }
+        
+        public static string Reverse(this string s )
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse( charArray );
+            return new string( charArray );
+        }
+    }
+}
