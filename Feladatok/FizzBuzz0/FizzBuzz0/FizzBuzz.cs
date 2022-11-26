012e023b75dbeb9cc33445cb33b72e7272759b52
Alex_sz1 <61713353+TheTalkingWeed@users.noreply.github.com>
2022-11-26 14:00:05 +0100

    Add files via upload

---
+++ Feladatok/FizzBuzz0/FizzBuzz0/FizzBuzz.cs
@@ -0,0 +1,27 @@
+﻿namespace FizzBuzz0;
+
+public class FizzBuzz
+{
+
+    private int range;
+
+    public FizzBuzz(int n)
+    {
+        range = n;
+    }
+
+    public void Start()
+    {
+        string output;
+        for (int i = 1; i <= this.range; i++)
+        {
+            output = i % 3 == 0 && i % 5 == 0
+                ? "fizz buzz" : i % 3 == 0
+                ? "fizz" : i % 5 == 0
+                ? "buzz" : i.ToString();
+            
+            Console.Out.WriteLine(output);
+                
+        }
+    }
+}
---
+++ Feladatok/FizzBuzz0/FizzBuzz0/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FizzBuzz0
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            FizzBuzz fb = new FizzBuzz(100);
+            fb.Start();
+        }
+    }
+}
+
