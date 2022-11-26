59a2060b7529346c7ab38a9ccd4bc264a95464ef
Alex_sz1 <61713353+TheTalkingWeed@users.noreply.github.com>
2022-11-26 14:00:54 +0100

    Add files via upload

---
+++ Feladatok/LessThan1000/LessThan1000/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+
+namespace LessThan100
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Out.WriteLine(CiklusosMegoldas(1000));
+            Out.WriteLine(LinQMegoldas(1000));
+            
+        }
+
+        public static int CiklusosMegoldas(int n)
+        {
+            int res = 0;
+            for (int i = 0; i < 1000; i++)
+                if (i % 3 == 0 || i % 5 == 0)
+                    res += i;
+
+            return res;
+        }
+
+        public static int LinQMegoldas(int n)
+        {
+            return Enumerable.Range(0,n).Where(x => x % 3 == 0 || x % 5 == 0).Sum();
+        }
+    }
+}
