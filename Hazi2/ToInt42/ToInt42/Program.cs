11d11d6b3bc29bc0c49c4eacc98a6f833ce9d86d
Alex_sz1 <61713353+TheTalkingWeed@users.noreply.github.com>
2022-12-04 21:09:30 +0100

    Add files via upload

---
+++ Hazi2/ToInt42/ToInt42/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ToInt42
+{
+    static class Program
+    {
+        public static void Main(string[] args)
+        {
+            string s = "42";
+            int i = s.ToInt();
+            Console.Out.WriteLine(i);
+        }
+        public static int ToInt(this string str)
+        {
+            return int.Parse(str);
+        }
+    }
+}
+
+
